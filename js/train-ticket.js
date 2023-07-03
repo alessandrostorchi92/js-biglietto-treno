8460c74bc849bbcb5bf258cf7edbddaf7d861e6e
alessandrostorchi92 <storchi.alle@gmail.com>
2023-07-03 09:15:25 +0200

    Coupon

--- js/train-ticket.js
+++ js/train-ticket.js
@@ -2,6 +2,10 @@ const itineraryLength = prompt("Inserisci la distanza in km da percorrere");
 const ageUser = prompt("Inserisci la tua età");
 const ticketPrice = 0.21 * parseInt(itineraryLength);
 const priceInCent = ticketPrice.toFixed(2)
+const couponChildren = (ticketPrice.toFixed(2)) * 0.2
+const couponElders = (ticketPrice.toFixed(2)) * 0.4
 
 console.log(ticketPrice)
 console.log(priceInCent)
+console.log("couponChildren", couponChildren)
+console.log("couponElders", couponElders)
